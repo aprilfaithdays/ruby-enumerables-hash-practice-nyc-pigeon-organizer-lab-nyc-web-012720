04667964e3041c33e8a33d158b26169a5de6b66c
April Escobar <aprilfaith.e@gmail.com>
2020-01-22 21:10:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,15 @@ def nyc_pigeon_organizer(data)
   reorganized = {}
   
   data.each do |pigeon_name, description|
-    description.each do |
-  
+    description.each do |keys, values|
+      values.each do |name|
+        reorganized[name] = {
+          :color => [],
+          :gender => [],
+          :lives => []
+        }
+      end
+    end
+  end
   reorganized
 end
