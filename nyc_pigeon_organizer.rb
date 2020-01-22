bcb5c50926b574bf4a00fc26aa364c3ba032be81
April Escobar <aprilfaith.e@gmail.com>
2020-01-22 21:25:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,9 +16,9 @@ def nyc_pigeon_organizer(data)
   final_keys = reorganized.keys
   
   data[:color].each do |bird_color, bird_name|
-    name.each do |name|
+    name.each do |bird_name|
       final_keys.each do |item|
-        if name === item
+        if bird_name === item
           reorganized[item][:color] << bird_color.to_s
         end
       end
